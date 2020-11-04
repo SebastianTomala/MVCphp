0bc89e6723021fed5443d7c2f2ca192a98cf380d
Sebastian Tomala <sebastian.tomalas@gmail.com>
2020-11-04 20:48:20 +0100

    forms fixes

--- app/views/users/info.php
+++ app/views/users/info.php
@@ -11,9 +11,33 @@
                         <img width="200" height="200" src="https://via.placeholder.com/300" alt="avatar">
                     </div>
                     <div class="data">
-                        <div class="item email"><span class="title">E-mail: </span><span class="value"><?php echo $data['user_email']; ?></span><a href="<?php echo URLROOT; ?>/users/edit/<?php echo $data['user_id']; ?>" class="btn btn-edit">edit</a></div>
-                        <div class="item phone"><span class="title">Phone: </span><span class="value"><?php echo $data['user_email']; ?></span><a href="<?php echo URLROOT; ?>/users/edit/<?php echo $data['user_id']; ?>" class="btn btn-edit">edit</a></div>
-                        <div class="item password"><span class="title">Password: </span><span class="value"><?php echo $data['user_email']; ?></span><a href="<?php echo URLROOT; ?>/users/edit/<?php echo $data['user_id']; ?>" class="btn btn-edit">edit</a></div>
+                        <div class="item email">
+                            <span class="title">E-mail: </span>
+                            <span class="value"><?php echo $data['user_email']; ?></span>  
+                            <?php if($data['user_id'] == $_SESSION['user_id']): ?>
+                                <a href="<?php echo URLROOT; ?>/users/edit/<?php echo $data['user_id']; ?>" class="btn btn-edit">
+                                edit
+                                </a>
+                            <?php endif; ?>
+                        </div>
+                        <div class="item phone">
+                            <span class="title">Phone: </span>
+                            <span class="value"><?php echo $data['user_email']; ?></span>
+                            <?php if($data['user_id'] == $_SESSION['user_id']): ?>
+                                <a href="<?php echo URLROOT; ?>/users/edit/<?php echo $data['user_id']; ?>" class="btn btn-edit">
+                                edit
+                                </a>
+                            <?php endif; ?>
+                        </div>
+                        <div class="item password">
+                            <span class="title">Password: </span>
+                            <span class="value"><?php echo $data['user_email']; ?></span>
+                            <?php if($data['user_id'] == $_SESSION['user_id']): ?>
+                                <a href="<?php echo URLROOT; ?>/users/edit/<?php echo $data['user_id']; ?>" class="btn btn-edit">
+                                edit
+                                </a>
+                            <?php endif; ?>
+                        </div>
                     </div>
                 </div>
             </section>
